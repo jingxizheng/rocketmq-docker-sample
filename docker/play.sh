4a6c13d193ff87c4eae465174715d3c19cce4c62
zhengjx <zhengjx@linesum.com>
2017-07-31 14:11:49 +0800

    change Dockerfile in broker from CMD to RUN

--- docker/play.sh
+++ docker/play.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 sudo docker build -t apache/incubator-rocketmq-base:4.0.0-incubating ./base
 
-sudo docker run -d -p 9876:9876 --name rmqnamesrv  apache/incubator-rocketmq-base:4.0.0-incubating
+sudo docker build -t apache/incubator-rocketmq-namesrv:4.0.0-incubating ./namesrv
 
-sudo docker run -d -p 10911:10911 -p 10909:10909 --name rmqbroker --link rmqnamesrv:rmqnamesrv -e "NAMESRV_ADDR=rmqnamesrv:9876" apache/incubator-rocketmq-base:4.0.0-incubating
+sudo docker build -t apache/incubator-rocketmq-broker:4.0.0-incubating ./broker
+
+sudo docker run -d -p 9876:9876 --name rmqnamesrv  apache/incubator-rocketmq-namesrv:4.0.0-incubating
+
+sudo docker run -d -p 10911:10911 -p 10909:10909 --name rmqbroker --link rmqnamesrv:rmqnamesrv -e "NAMESRV_ADDR=rmqnamesrv:9876" apache/incubator-rocketmq-broker:4.0.0-incubating
